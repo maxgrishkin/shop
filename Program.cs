304cc6e10ebdd082830a295925cc9cd1e3883a55
maxgrishkin <maxgrishkin2014@gmail.com>
2025-05-24 22:22:55 +0700

    фвывфыв

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace Project
                 new Product(){  Title = "Tomato", Price = 120 },
                 new Product(){  Title = "Potato", Price = 40},
                 new Product(){  Title = "Potato", Price = 40},
-                new Product(){  Title = "Banana", Price = 500},
+                new Product(){  Title = "Ананас", Price = 500},
             });
 
             foreach (Product product in shop.GetProducts())
